60c7fb6188213e45e01b020564c1734aa097b35d
Sweet Zhang <80295133+SweetZhang447@users.noreply.github.com>
2026-08-21 11:56:15 -0700

    Remove duplicate print statements
    Duplicate print statements in the main function.

--- dummy.py
+++ dummy.py
@@ -3,5 +3,9 @@ def main():
   print("yay!")
   print("browse_code repro PRASS-2622")
 
+  print("change to main")
+  print("yay!")
+  print("browse_code repro PRASS-2622")
+
 if __name__ == "__main__":
   main()
