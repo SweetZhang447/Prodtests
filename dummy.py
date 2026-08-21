ac3e2adb5430aa9c8b835b154d2d0d04315421af
Sweet Zhang <80295133+SweetZhang447@users.noreply.github.com>
2026-08-21 12:04:15 -0700

    Add 'tilted' function to print numbers with repetition
    Added a new function 'tilted' that prints numbers from 0 to 9 and repeats each number while 'a' is True.

--- dummy.py
+++ dummy.py
@@ -1,7 +1,17 @@
+import numpy as np
+
 def main():
   print("change to main")
   print("yay!")
   print("browse_code repro PRASS-2622")
 
+def tilted():
+  a = True
+  for i in np.arange(0,10):
+    print(i)
+    while a:
+      print(i)
+
+  a = False
 if __name__ == "__main__":
   main()
